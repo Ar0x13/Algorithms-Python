5a0b6d502667ad8d3ea002b9327315f7d7ab89eb
Andrey Ruban <16754503+Ar0x13@users.noreply.github.com>
2020-11-20 20:16:41 +0200

    bubble_sort (final) - v2.0

--- bubble_sort.py
+++ bubble_sort.py
@@ -7,17 +7,10 @@ Bubble sorting in python3
 import random
 
 numbers_list = [random.randint(1, 15) for x in range(1,15)]
-print("start_list:", numbers_list)
 
-for counter in range(1, len(numbers_list) - 1):
-  i = 0
-  for i in range(i, len(numbers_list) - 1):
-    if numbers_list[i] > numbers_list[i+1]:
-      numbers_list[i], numbers_list[i+1] = numbers_list[i+1], numbers_list[i]
-      i += 1
-    elif numbers_list[i] <= numbers_list[i+1]:
-      continue
-    else:
-      break
-
-print("final_list:", numbers_list)
+print("my_start_list: \n",  numbers_list)
+for i in range(len(numbers_list) - 1):
+  for j in range(len(numbers_list) - 1):
+    if numbers_list[j] > numbers_list[j+1]:
+      numbers_list[j], numbers_list[j+1] = numbers_list[j+1], numbers_list[j]
+print("my_final_list: \n", numbers_list)
